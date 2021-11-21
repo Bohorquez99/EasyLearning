f50b08a5306722efa5a0b2f2fab28b6709082019
Sebastian <bohorquez3009@gmail.com>
2021-11-21 13:14:11 -0500

    CONTROLES

--- app/src/main/java/com/example/easylearning/JugarActivity.java
+++ app/src/main/java/com/example/easylearning/JugarActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+
 public class JugarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jugar);
+
     }
 }
